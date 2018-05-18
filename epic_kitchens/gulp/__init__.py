6e6e66bc5aae3ddc9c82cf955b4b18c165a1bf64
Will Price <will.price94@gmail.com>
2018-05-18 16:36:23 +0100

    [docs] Improve docstring for epic_kitchens.gulp

--- epic_kitchens/gulp/__init__.py
+++ epic_kitchens/gulp/__init__.py
@@ -1,5 +1,6 @@
-"""
-Dataset Adapters for GulpIO. This module contains two adapters for 'gulping' both RGB and flow frames
+""" Dataset Adapters for GulpIO.
+
+This module contains two adapters for 'gulping' both RGB and flow frames
 which can then be used with the ``EpicVideoDataset`` classes.
 """
 
