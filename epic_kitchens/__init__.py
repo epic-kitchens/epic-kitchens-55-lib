3b17ca654cd58690352628157ab8c6796570fdda
Will Price <will.price94@gmail.com>
2018-05-18 16:35:43 +0100

    [docs] Add docstring to epic_kitchens

--- epic_kitchens/__init__.py
+++ epic_kitchens/__init__.py
@@ -1,3 +1,12 @@
+""" ``epic-kitchens``
+
+This library contains a variety of useful classes and functions for performing common operations on
+the `EPIC Kitchens`_ dataset.
+
+.. _`EPIC Kitchens`: https://epic-kitchens.github.io/
+
+"""
+
 from . import gulp, dataset
 
 __version__ = '1.1.0rc0'
